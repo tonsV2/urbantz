a9c50f5ced12260c0efdf9f06e285af36b673e1a
sj <sebastianthegreatful@gmail.com>
2021-03-13 14:42:18 +0700

    Refactor path into a variable which can be shared among tests

--- src/tests/uptime.test.js
+++ src/tests/uptime.test.js
@@ -2,15 +2,17 @@ const request = require('supertest')
 const server = require('../server')
 
 describe('/uptime Endpoint', () => {
+    const path = '/uptime';
+
     it('should return a json response with a timestamp property', async () => {
         const res = await request(server)
-            .get('/uptime')
+            .get(path)
         expect(res.body).toHaveProperty('timestamp')
     })
 
     it('should return a json response with a uptime property', async () => {
         const res = await request(server)
-            .get('/uptime')
+            .get(path)
         expect(res.body).toHaveProperty('uptime')
     })
 })
